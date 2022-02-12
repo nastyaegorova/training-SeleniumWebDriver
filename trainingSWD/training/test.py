8013b076ce64f451257339959399073effcbdd97
nastyaegorova <egorova_nastya99@mail.ru>
2022-02-12 20:59:11 +0300

    Создание проекта. Тест поисковой строки Google

---
+++ trainingSWD/training/test.py
@@ -0,0 +1,21 @@
+import pytest
+import time
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+@pytest.fixture
+def driver(request):
+    wd = webdriver.Chrome(executable_path="D:\\chromedriver.exe")
+    print(wd.capabilities)
+    request.addfinalizer(wd.quit)
+    return wd
+
+
+def test_example(driver):
+    driver.get("http://www.google.com/")
+    driver.find_element_by_name("q").send_keys("webdriver")
+    time.sleep(1)
+    driver.find_element_by_name("btnK").click()
+    WebDriverWait(driver, 10).until(EC.title_is("webdriver - Поиск в Google"))
